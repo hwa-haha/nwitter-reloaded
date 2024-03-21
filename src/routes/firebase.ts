3ce88e93eee8d92754b59c0220adfbce6a4da3bd
hwayoung lee <hylee13ee@gmail.com>
2024-03-21 17:49:47 +0900

    feat: 계정정보및 트위터완성
    firebase 데이터베이스 연결

--- src/routes/firebase.ts
+++ src/routes/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBtJzoCrVPVJs0ZrQ9GufZ5kD5Ihm6M3BM",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth =getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { auth } from "./firebase";
+
+import styled from "styled-components";
+import PostTweetForm from "../component/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 export default function Home() {
-    const logOut = () => {
-        auth.signOut();
-    }
     return (
-        <h1>
-            <button onClick={logOut}>Log out</button>
-        </h1>
+        <Wrapper>
+        <PostTweetForm />
+        </Wrapper>
     );
 }
 
